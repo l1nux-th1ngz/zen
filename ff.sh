435739752243d3a66d4da2282aa7597bc84bbd9a
l1nux-th1ngz <137152321+l1nux-th1ngz@users.noreply.github.com>
2023-12-19 11:49:43 -0700

    Update ff.sh

--- ff.sh
+++ ff.sh
@@ -1,5 +1,5 @@
+
 import tkinter as tk
-import os
 from tkinter import messagebox
 from tkcalendar import Calendar
 from time import strftime
@@ -8,7 +8,7 @@ class Window:
     def __init__(self):
         self.root = tk.Tk()
         self.root.title("Package Installation Options")
-        self.root.configure(bg='#2196F3')  # Set background color
+        self.root.configure(bg='#00A5FF')  # Set background color
         self.widgets_builder()
         # Grab the saved data
         getdata(self)
@@ -20,28 +20,38 @@ class Window:
         self.entry1()
         self.listbox()
         self.label2()
+        self.calendar()
+        self.message_boxes()
 
     def label1(self):
         self.lab1 = tk.Label(
             self.root, text="Enter to add items, \nSelect to delete Items,\n ctr+s to save",
-            bg="#ff00d0")  # Set label background color
-        self.lab1.pack(fill="both")
+            bg='#FF00D0', anchor="w")  # Set label background color and anchor to left
+        self.lab1.pack(fill="both", padx=5, pady=5)  # Add space between border and label
 
     def entry1(self):
         self.v = tk.StringVar()
         self.entry = tk.Entry(self.root, textvariable=self.v, font=24)
-        self.entry.pack(fill="both")
+        self.entry.pack(fill="both", padx=5, pady=5)  # Add space between border and entry box
         self.entry.focus()
 
     def listbox(self):
         self.lv = tk.Variable()
-        self.lst = tk.Listbox(self.root, listvariable=self.lv, height=10, selectbackground='#00a5ff')  # Set listbox height and selection color
-        self.lst.pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
+        self.lst = tk.Listbox(self.root, listvariable=self.lv, height=10, selectbackground='#AE00FF')  # Set listbox height and selection color
+        self.lst.pack(side=tk.LEFT, fill=tk.BOTH, expand=1, padx=5, pady=5)  # Add space between border and listbox
 
     def label2(self):
         self.label = tk.Label(self.root, text="Press enter to add an item",
-            bg="#ae00ff")  # Set label background color
-        self.label.pack(fill="both")
+            bg='#FF00D0', anchor="w")  # Set label background color and anchor to left
+        self.label.pack(fill="both", padx=5, pady=5)  # Add space between border and label
+
+    def calendar(self):
+        cal = Calendar(self.root, selectmode="day", year=2023, month=1, day=1, bg='#00A5FF', fg='white', height=6)  # Set calendar height and background color
+        cal.pack(side=tk.LEFT, padx=5, pady=5)  # Add space between border and calendar
+
+    def message_boxes(self):
+        self.message1 = tk.Label(self.root, text="Copy to .config", bg='#FF00D0', anchor="w")  # Set label background color and anchor to left
+        self.message1.pack(fill="both", padx=5, pady=5)  # Add space between border and message box
 
     def binding(self):
         self.lst.bind("<Double-Button>", lambda x: delete(x, self))
@@ -57,11 +67,11 @@ class PackageInstaller:
             messagebox.showinfo("Copy to .config", "Directory copied to .config for faster and easier access.")
 
     def show_calendar(self):
-        cal = Calendar(self.window.root,  month=1, selectmode="day", year=2023, day=1, bg='#282828', fg='white')
+        cal = Calendar(self.window.root, selectmode="day", year=2023, month=1, day=1, bg='#00A5FF', fg='white')
         cal.pack()
 
     def update_time(self):
-        string = strftime('%H:%M:%S %P')
+        string = strftime('%H:%M:%S %p')
         self.window.time_label.config(text=string)
         self.window.time_label.after(1000, self.update_time)
 
